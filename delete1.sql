43f81e7beef0fb1cc340d9aa25d59011c6f82885
Devika <devikanaird@gmail.com>
2021-06-20 01:12:09 +0530

    delete 1 initial

---
+++ delete1.sql
@@ -0,0 +1,8 @@
+--Write a file called delete1.sql that contains a SQL statement to delete the subject 'History'. The subject must be removed from the tables and from all the books. You will need two queries for this. First to delete all the history mappings in the books_subjects table. Then to delete the subject itself. To do the former, you can use subqueries (e.g. delete from books_subject where id in (...))
+
+DELETE FROM books_subjects
+	WHERE subject IN(
+	SELECT s.id
+	FROM subjects s, books_subjects bs
+	WHERE s.id = bs.subject and s.name = 'History');
+	
