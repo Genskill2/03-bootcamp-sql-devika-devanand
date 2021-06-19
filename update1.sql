59198be13006c536942b97da8da7f110694cd367
Devika <devikanaird@gmail.com>
2021-06-20 01:11:32 +0530

    Update 1 initial

---
+++ update1.sql
@@ -0,0 +1,4 @@
+--Write a file called update1.sql which will contain a SQL statement to change the name of the publisher PHI to "Prentice Hall"
+
+UPDATE publisher SET name = 'Prentice Hall' WHERE name = 'PHI';
+
