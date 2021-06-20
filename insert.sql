cce7dfe7c157e6a2955da7f678094414acc361c2
Devika <devikanaird@gmail.com>
2021-06-20 11:24:43 +0530

    insert 4

--- insert.sql
+++ insert.sql
@@ -1,3 +1,25 @@
+--Entering values for publisher table
+INSERT INTO publisher(id,name,country) VALUES (1,'PHI','India');
+INSERT INTO publisher(id,name,country) VALUES (2,'Harper','USA');
+INSERT INTO publisher(id,name,country) VALUES (3,'GCP','USA');
+INSERT INTO publisher(id,name,country) VALUES (4,'Avery','USA');
+INSERT INTO publisher(id,name,country) VALUES (5,'Del Rey','UK');
+INSERT INTO publisher(id,name,country) VALUES (6,'Vintage','UK');
+
+
+--Entering values for books
+INSERT INTO books(id,title,publisher) VALUES (1,'The C Programming Language',1);
+INSERT INTO books(id,title,publisher) VALUES (2,'The Go Programming Language',1);
+INSERT INTO books(id,title,publisher) VALUES (3,'The UNIX Programming Environment',1);
+INSERT INTO books(id,title,publisher) VALUES (4,'Cryptonomicon',2);
+INSERT INTO books(id,title,publisher) VALUES (5,'Deep Work',3);
+INSERT INTO books(id,title,publisher) VALUES (6,'Atomic Habits',4);
+INSERT INTO books(id,title,publisher) VALUES (7,'The City and The City',5);
+INSERT INTO books(id,title,publisher) VALUES (8,'The Great War for Civilisation',6);
+
+
+
+
 --Entering each subject into the subjects table
 INSERT INTO subjects(id,name) VALUES (1,'C');
 INSERT INTO subjects(id,name) VALUES (2,'UNIX');
@@ -10,15 +32,6 @@ INSERT INTO subjects(id,name) VALUES (8,'Politics');
 INSERT INTO subjects(id,name) VALUES (9,'History');
 
 
---Entering values for publisher table
-INSERT INTO publisher(id,name,country) VALUES (1,'PHI','India');
-INSERT INTO publisher(id,name,country) VALUES (2,'Harper','USA');
-INSERT INTO publisher(id,name,country) VALUES (3,'GCP','USA');
-INSERT INTO publisher(id,name,country) VALUES (4,'Avery','USA');
-INSERT INTO publisher(id,name,country) VALUES (5,'Del Rey','UK');
-INSERT INTO publisher(id,name,country) VALUES (6,'Vintage','UK');
-
-
 --Entering values into books_subjects
 INSERT INTO books_subjects(book,subject) VALUES (1,1),(1,2),(1,3);
 INSERT INTO books_subjects(book,subject) VALUES (2,4),(2,3);
@@ -28,17 +41,6 @@ INSERT INTO books_subjects(book,subject) VALUES (5,3),(5,6);
 INSERT INTO books_subjects(book,subject) VALUES (6,6),(6,7);
 INSERT INTO books_subjects(book,subject) VALUES (7,5),(7,8);
 INSERT INTO books_subjects(book,subject) VALUES (8,8),(8,9);
-
-
---Entering values for books
-INSERT INTO books(id,title,publisher) VALUES (1,'The C Programming Language',1);
-INSERT INTO books(id,title,publisher) VALUES (2,'The Go Programming Language',1);
-INSERT INTO books(id,title,publisher) VALUES (3,'The UNIX Programming Environment',1);
-INSERT INTO books(id,title,publisher) VALUES (4,'Cryptonomicon',2);
-INSERT INTO books(id,title,publisher) VALUES (5,'Deep Work',3);
-INSERT INTO books(id,title,publisher) VALUES (6,'Atomic Habits',4);
-INSERT INTO books(id,title,publisher) VALUES (7,'The City and The City',5);
-INSERT INTO books(id,title,publisher) VALUES (8,'The Great War for Civilisation',6);
 
 
 
